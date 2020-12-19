c3fa58dc9150796d6888df690e8931b049c72e6c
mehak <mehak@yopmail.com>
2020-12-19 23:15:21 +0530

    Add files to previous commit

---
+++ src/Views/About.js
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <div class>
+            <h1 className="font-bold text-2xl mb-3">
+                About Us
+            </h1>
+            <p>
+                This is the about page content.
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Views/Home.js
@@ -0,0 +1,14 @@
+import HelloWorld from "../Components/HelloWorld";
+
+function Home() {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl">
+                This is the Home Page
+            </h1>
+            <HelloWorld />
+        </div>
+    )
+}
+
+export default Home
