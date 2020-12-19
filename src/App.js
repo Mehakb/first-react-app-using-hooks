eef69f0936abfa8bfd85a932b7e1723ee40bc89e
mehak <mehak@yopmail.com>
2020-12-19 21:33:46 +0530

    create header, footer and menu.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import HelloWorld from './Components/HelloWorld';
+// import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HelloWorld />
+      <Footer />
+      {/* <CounterExample /> */}
+    </div>
+  );
+}
+
+export default App;
