2617e08cd847f1e1d28677f90519df5930c672ea
mehak <mehak@yopmail.com>
2020-12-19 23:14:40 +0530

    Apply Routes to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import HelloWorld from './Components/HelloWorld';
 // import CounterExample from './Components/CounterExample';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Views/Home';
+import About from './Views/About';
 
 function App() {
   return (
     <div>
+      <Router>
       <Header />
-      <HelloWorld />
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
       <Footer />
+      </Router>
       {/* <CounterExample /> */}
     </div>
   );
